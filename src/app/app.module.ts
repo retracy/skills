ff24af8d9b53d20016db36c7118b491102cbc60f
Emily Tracy <emgoheen@yahoo.com>
2022-06-14 12:40:22 -0700

    Added newly supported Angular14 dynamic tab titles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, TitleStrategy } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SkillsTitlePageStrategy } from './services/title-strategy.service';
 
 const routes: Routes = [
   { path: 'home', loadChildren: () => import('./components/skills.module').then(m => m.SkillsModule)},
@@ -21,7 +22,7 @@ const routes: Routes = [
     HttpClientModule,
     RouterModule.forRoot(routes),
   ],
-  providers: [],
+  providers: [{provide: TitleStrategy, useClass: SkillsTitlePageStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
