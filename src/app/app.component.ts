898675abce8ee2c27ee569110125fa2c331c136c
Emily Tracy <emgoheen@yahoo.com>
2023-05-05 16:48:25 -0700

    Updates to work history and some additional settings for code formatting.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
-import { ChangeDetectionStrategy, Component, OnInit, VERSION } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  VERSION,
+} from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent implements OnInit {
-  constructor(public title: Title) { }
+  constructor(public title: Title) {}
 
   ngOnInit() {
-    this.title.setTitle(`Our Skills ${VERSION.full}`)
+    this.title.setTitle(`Our Skills ${VERSION.full}`);
   }
 }
